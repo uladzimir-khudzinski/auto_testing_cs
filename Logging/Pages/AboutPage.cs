8801ff8761861e45b6e6025f6457cf08016ca31f
Uladzimir Khudzinski <uladzimir.khudzinski@softswiss.com>
2025-11-20 19:53:46 +0100

    Add log4net logging and enhance test observability

--- Logging/Pages/AboutPage.cs
+++ Logging/Pages/AboutPage.cs
@@ -1,10 +1,12 @@
-using OpenQA.Selenium;
+using log4net;
 using Logging.TestData;
+using OpenQA.Selenium;
 
 namespace Logging.Pages;
 
 public class AboutPage : BasePage
 {
+    private static readonly ILog logger = LogManager.GetLogger(typeof(ContactPage));
     private readonly By contentTitle = By.XPath(
         "//div[@class='content']//h1[@class='subheader__title']"
     );
@@ -14,11 +16,13 @@ public class AboutPage : BasePage
 
     public string GetContentTitle()
     {
+        logger.Info("Retrieving content title from About page.");
         return Driver.FindElement(contentTitle).Text;
     }
 
     public bool IsAtExpectedPage()
     {
+        logger.Info("Verifying if the About page is at the expected URL, title, and content title.");
         return CurrentUrl == EhuTestData.Urls.About
             && PageTitle == EhuTestData.ExpectedTitles.About
             && GetContentTitle() == EhuTestData.ExpectedTitles.About;
--- Logging/Pages/SearchResultsPage.cs
+++ Logging/Pages/SearchResultsPage.cs
@@ -1,10 +1,12 @@
-using System;
+using log4net;
 using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace Logging.Pages;
 
 public class SearchResultsPage : BasePage
 {
+    private static readonly ILog logger = LogManager.GetLogger(typeof(ContactPage));
     private readonly string query;
 
     public SearchResultsPage(string query)
@@ -14,6 +16,7 @@ public class SearchResultsPage : BasePage
 
     public bool UrlContainsQuery()
     {
+        logger.Info($"Verifying if the URL contains the search query: {query}");
         var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
         var normalizedQuery = Uri.EscapeDataString(query.Trim()).Replace("%20", "+");
         return wait.Until(d => d.Url.Contains($"/?s={normalizedQuery}", StringComparison.OrdinalIgnoreCase));
